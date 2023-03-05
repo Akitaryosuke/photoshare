5b739b6f80dfbd2f2da2b31ffbd60f34990838c1
EC2 Default User <ec2-user@ip-172-31-37-229.ap-northeast-1.compute.internal>
2023-03-05 11:35:11 +0000

    update

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -16,32 +16,12 @@ class PostController < ApplicationController
       render :new
     end
   end
-  
-  # 画像アップロード用のアクション
-  def upload_image
-    @image_blob = create_blob(params[:image])
-    render json: @image_blob
-  end
 
   
   private
 
   def post_params
-    params.require(:post).merge(images: uploaded_images)
-  end
-  
-  # アップロード済み画像の検索
-  def uploaded_images
-    params[:post][:images].drop(1).map{|id| ActiveStorage::Blob.find(id)} if params[:post][:images]
-  end
-
-  # blobデータの作成
-  def create_blob(file)
-    ActiveStorage::Blob.create_and_upload!(
-      io: file.open,
-      filename: file.original_filename,
-      content_type: file.content_type
-    )
+    params.require(:post).permit(:image)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   root to: "post#index"
   
   resources :post, only: [:new, :index, :create]
-  post "posts/upload_image", to: "posts#upload_image"
-  
 end
